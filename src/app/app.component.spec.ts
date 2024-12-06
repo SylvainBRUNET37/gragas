36d47eabe5874e64950378e63fe61e8f91e5ff18
matisdbs37 <matisdubois18@gmail.com>
2024-12-06 01:59:33 +0100

    bomba

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'gragas' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gragas');
   });
 
   it('should render title', () => {
